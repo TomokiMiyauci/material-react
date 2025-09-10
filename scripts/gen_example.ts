6b27de0b9a6c79c57fbd322fbadf742cb3661358
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-10 17:16:10 +0900

    chore: define material design token, generator for css

---
+++ scripts/gen_example.ts
@@ -0,0 +1,36 @@
+import { formats, transformGroups } from "style-dictionary/enums";
+import type { Config } from "style-dictionary";
+import { fromFileUrl } from "@std/path";
+import instance, { transformers } from "~/resources/style_dictionary/mod.ts";
+
+const resource = {
+  from: fromFileUrl(import.meta.resolve("~/resources/token.json")),
+  to: fromFileUrl(import.meta.resolve("~/examples/material_design.css")),
+};
+
+const config = {
+  source: [resource.from],
+  platforms: {
+    css: {
+      transformGroup: transformGroups.css,
+      files: [{
+        destination: resource.to,
+        format: formats.cssVariables,
+        options: {
+          showFileHeader: false,
+          outputReferences: true,
+        },
+      }],
+      transforms: [
+        transformers.typography,
+        transformers.token,
+        transformers.dimension,
+      ],
+    },
+  },
+} satisfies Config;
+
+if (import.meta.main) {
+  const sd = await instance.extend(config);
+  await sd.buildAllPlatforms();
+}
