ad42c965b825a2001675dcac02584b444fd70f77
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-12 21:05:46 +0900

    chore: set default background value

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,13 @@
-import type { Preview } from "@storybook/react-vite";
+/// <reference lib="dom" />
 
+import type { Preview } from "@storybook/react-vite";
 import "./global.css";
-export default {} satisfies Preview;
+import "../examples/material.css";
+
+const isDark = globalThis.matchMedia("(prefers-color-scheme: dark)").matches;
+
+export default {
+  initialGlobals: {
+    backgrounds: { value: isDark ? "dark" : undefined },
+  },
+} satisfies Preview;
