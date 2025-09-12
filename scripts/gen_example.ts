29cc35ea435896c1854064cf2c621e6194ee89df
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-12 17:52:39 +0900

    chore(examples): add dark theme color

--- scripts/gen_example.ts
+++ scripts/gen_example.ts
@@ -1,37 +1,102 @@
-import { formats, transformGroups } from "style-dictionary/enums";
+import { formats, transformGroups, transforms } from "style-dictionary/enums";
 import type { Config } from "style-dictionary";
+import type { File, Transform } from "style-dictionary/types";
+import StyleDictionary from "style-dictionary";
 import { fromFileUrl } from "@std/path";
 import instance, { transformers } from "~/resources/style_dictionary/mod.ts";
 
-const resource = {
-  from: fromFileUrl(import.meta.resolve("~/resources/token.json")),
-  to: fromFileUrl(import.meta.resolve("~/examples/material_design.css")),
-};
+type Theme = "light" | "dark";
+type Contrust = "default";
+
+const themes = ["light", "dark"] satisfies Theme[];
+const contrusts = ["default"] satisfies Contrust[];
+
+const kebabFlatternTransformer = {
+  type: "name",
+  name: "name/kebab+flatten",
+  transform: (token, config, options, vol) => {
+    const kebabTransformer =
+      StyleDictionary.hooks.transforms[transforms.nameKebab];
+    const newToken = {
+      ...token,
+      path: token.path.filter((value) => !value.startsWith("$")),
+    };
+
+    const result = kebabTransformer?.transform(newToken, config, options, vol);
+
+    if (typeof result === "string") return result;
+
+    throw new Error();
+  },
+} satisfies Transform;
+
+const baseFile = {
+  format: formats.cssVariables,
+  options: {
+    showFileHeader: false,
+    outputReferences: true,
+  },
+} satisfies Pick<File, "format" | "options">;
+
+function makeFile(theme: Theme, contrust: Contrust): File {
+  const base = {
+    ...baseFile,
+    format: formats.cssVariables,
+    options: {
+      showFileHeader: false,
+      outputReferences: true,
+    },
+    filter: ({ path }) => {
+      return path.includes(`$${theme}`) && path.includes(`$${contrust}`);
+    },
+  } satisfies Pick<File, "format" | "options" | "filter">;
+
+  switch (contrust) {
+    case "default": {
+      return {
+        ...base,
+        destination: `${theme}.css`,
+      };
+    }
+
+    default: {
+      return {
+        ...base,
+        destination: "hoge",
+      };
+    }
+  }
+}
 
 const config = {
-  source: [resource.from],
+  source: [fromFileUrl(import.meta.resolve("~/resources/token.json"))],
   platforms: {
     css: {
       transformGroup: transformGroups.css,
-      files: [{
-        destination: resource.to,
-        format: formats.cssVariables,
-        options: {
-          showFileHeader: false,
-          outputReferences: true,
+      buildPath: fromFileUrl(import.meta.resolve("~/examples/")),
+      files: themes.flatMap((theme) =>
+        contrusts.map((contrust) => makeFile(theme, contrust))
+      ).concat({
+        ...baseFile,
+        destination: "material_design.css",
+        filter: ({ path }) => {
+          return path.every((value) => !value.startsWith("$"));
         },
-      }],
+      }),
       transforms: [
         transformers.typography,
         transformers.token,
         transformers.dimension,
         transformers.color,
+        kebabFlatternTransformer.name,
       ],
     },
   },
+  log: { verbosity: "silent" },
 } satisfies Config;
 
 if (import.meta.main) {
   const sd = await instance.extend(config);
+  sd.registerTransform(kebabFlatternTransformer);
   await sd.buildAllPlatforms();
 }
