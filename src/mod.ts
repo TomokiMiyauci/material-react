8d860f6d8efafd7d308cbac73123d0553311574e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-15 14:45:59 +0900

    chore: export badge component to default entry

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,4 @@
 export * from "./components/icon_button/mod.ts";
 export * from "./components/navigation_bars/navigation_bar/mod.ts";
 export * from "./components/navigation_bars/navigation_bar_item/mod.ts";
+export * from "./components/badge/mod.ts";
