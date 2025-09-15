ecd4a928ed8b3ad9425a29548690455a20cf3b94
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-15 12:56:16 +0900

    chore: apply dark mode to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,19 @@
 /// <reference lib="dom" />
 
 import type { Preview } from "@storybook/react-vite";
+import { themes } from "@storybook/theming";
+
 import "./global.css";
-import "../examples/material.css";
+import "../examples/material_design/style.css";
 
 const isDark = globalThis.matchMedia("(prefers-color-scheme: dark)").matches;
 
 export default {
+  parameters: {
+    docs: {
+      theme: isDark ? themes.dark : undefined,
+    },
+  },
   initialGlobals: {
     backgrounds: { value: isDark ? "dark" : undefined },
   },
