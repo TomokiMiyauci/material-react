7539e36e056b00b659c22f3ee42d56b9fc988323
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-20 16:38:38 +0900

    chore: add deploying css variables

--- scripts/postcss/inline_token.ts
+++ scripts/postcss/inline_token.ts
@@ -26,6 +26,8 @@ export default function inlineTokens(): Plugin {
         }
       });
 
+      const resolvedTokens = resolveVars(tokens);
+
       root.walkDecls((decl) => {
         const parsed = valueParser(decl.value);
 
@@ -33,8 +35,8 @@ export default function inlineTokens(): Plugin {
           if (node.type === "function" && node.value === "var") {
             const varName = node.nodes[0]?.value;
 
-            if (varName && tokens.has(varName)) {
-              const value = tokens.get(varName)!;
+            if (varName && resolvedTokens.has(varName)) {
+              const value = resolvedTokens.get(varName)!;
               // deno-lint-ignore no-explicit-any
               node.type = "word" as any;
               node.value = value;
@@ -51,3 +53,53 @@ export default function inlineTokens(): Plugin {
 inlineTokens.postcss = true;
 
 const flag = /^@embed$/;
+
+function resolveVars(vars: Map<string, string>): Map<string, string> {
+  function resolveOne(
+    key: string,
+    vars: Map<string, string>,
+    cache: Map<string, string>,
+  ): string | undefined {
+    if (cache.has(key)) return cache.get(key)!;
+
+    const raw = vars.get(key);
+    if (!raw) return;
+
+    const ast = valueParser(raw);
+
+    ast.walk((node) => {
+      if (node.type === "function" && node.value === "var") {
+        const refName = node.nodes[0]?.value;
+        if (!refName) return;
+
+        const resolved = resolveOne(refName, vars, cache);
+
+        if (resolved) {
+          // deno-lint-ignore no-explicit-any
+          node.type = "word" as any;
+          node.value = resolved;
+        }
+      } else {
+        cache.set(key, raw);
+      }
+    });
+
+    const resolved = ast.toString();
+
+    cache.set(key, resolved);
+    return resolved;
+  }
+
+  const cache = new Map<string, string>();
+  const resolvedVars = new Map<string, string>();
+
+  for (const key of vars.keys()) {
+    const value = resolveOne(key, vars, cache);
+
+    if (value) {
+      resolvedVars.set(key, value);
+    }
+  }
+
+  return resolvedVars;
+}
