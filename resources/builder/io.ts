d6ebc1535b9556d9cd9bb6c28949f85c25630365
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-23 00:34:53 +0900

    chore(resources): use safe write feature

--- resources/builder/io.ts
+++ resources/builder/io.ts
@@ -1,10 +1,12 @@
 import type { IO } from "./types.ts";
+import { ensureFile } from "@std/fs";
 
 export class DenoIO implements IO {
   read(url: URL): Promise<string> {
     return Deno.readTextFile(url);
   }
-  write(url: URL, contents: string): Promise<void> {
+  async write(url: URL, contents: string): Promise<void> {
+    await ensureFile(url);
     return Deno.writeTextFile(url, contents);
   }
 }
