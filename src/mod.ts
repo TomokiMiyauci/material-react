be3d7d6cd1de50acb665edb21e929ba986747088
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-25 22:54:54 +0900

    feat: export chips from default

--- src/mod.ts
+++ src/mod.ts
@@ -2,3 +2,7 @@ export * from "./components/icon_button/mod.ts";
 export * from "./components/navigation_bars/navigation_bar/mod.ts";
 export * from "./components/navigation_bars/navigation_bar_item/mod.ts";
 export * from "./components/badge/mod.ts";
+export * from "./components/chips/assist_chip/mod.ts";
+export * from "./components/chips/filter_chip/mod.ts";
+export * from "./components/chips/input_chip/mod.ts";
+export * from "./components/chips/suggestion_chip/mod.ts";
