80e0bb8fd579e751d2d3e412a9bc38a1833efba6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-26 14:59:23 +0900

    chore: add global decorator for padding

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@
 
 import type { Preview } from "@storybook/react-vite";
 import { themes } from "@storybook/theming";
+import { createElement } from "react";
 
 import "./global.css";
 import "../examples/material_themes/style.css";
@@ -17,4 +18,12 @@ export default {
   initialGlobals: {
     backgrounds: { value: isDark ? "dark" : undefined },
   },
+  decorators: [
+    (Story) =>
+      createElement(
+        "div",
+        { style: { "padding": "1rem" } },
+        createElement(Story),
+      ),
+  ],
 } satisfies Preview;
