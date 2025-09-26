4b7b24018110275c19a6f5bda27169e5a09b6740
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-26 22:54:26 +0900

    chore(assist-chip): change assist chip label

--- src/components/chips/assist_chip/AssistChip.stories.tsx
+++ src/components/chips/assist_chip/AssistChip.stories.tsx
@@ -18,15 +18,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Enabled = {} satisfies Story;
+export const Enabled = {
+  args: {
+    label: "Enabled",
+  },
+} satisfies Story;
 export const Disabled = {
   args: {
     disabled: true,
+    label: "Disabled",
   },
 } satisfies Story;
 export const Hovered = {
   parameters: {
     pseudo: { hover: true },
+  },
+  args: {
+    label: "Hovered",
   },
 } satisfies Story;
 
@@ -34,11 +42,17 @@ export const Focused = {
   parameters: {
     pseudo: { focusVisible: true },
   },
+  args: {
+    label: "Focused",
+  },
 } satisfies Story;
 
 export const Pressed = {
   parameters: {
     pseudo: { active: true },
+  },
+  args: {
+    label: "Pressed",
   },
 } satisfies Story;
 
