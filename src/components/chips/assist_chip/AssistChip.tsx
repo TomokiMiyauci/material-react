44a754bb48cf36107034a7b61a532c25ea74588d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-26 12:23:04 +0900

    chore: debug

--- src/components/chips/assist_chip/AssistChip.tsx
+++ src/components/chips/assist_chip/AssistChip.tsx
@@ -30,7 +30,7 @@ export default function AssistChip(
         <span data-label="">{label}</span>
       </span>
 
-      <Style>{style}</Style>
+      <Style href="assist-chip">{style}</Style>
     </>
   );
 }
