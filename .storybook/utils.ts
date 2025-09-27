86563b492ab83f047a5fec88353badbdc97432f4
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-27 16:14:31 +0900

    chore: add figma on storybook addon

---
+++ .storybook/utils.ts
@@ -0,0 +1,68 @@
+import nodeIdJson from "~/resources/node_id.json" with { type: "json" };
+
+class MaterialDesignBuilder {
+  #url: URL;
+  constructor(baseURL: string) {
+    this.#url = new URL(baseURL);
+  }
+  figmaURL(
+    component: "filter-chip",
+    options: { state?: State },
+  ): string {
+    const id = getNodeId(component, options);
+
+    const url = new URL(this.#url);
+    url.searchParams.set("node-id", id);
+
+    return url.toString();
+  }
+}
+
+type State =
+  | "enabled"
+  | "disabled"
+  | "hovered"
+  | "focused"
+  | "dragged"
+  | "pressed";
+
+function getNodeId(
+  component: "filter-chip",
+  options: { state?: State },
+): string {
+  switch (component) {
+    case "filter-chip": {
+      const outlined = nodeIdJson.map["filter-chip"].outlined;
+      switch (options.state) {
+        case "enabled":
+          return outlined.enabled;
+
+        case "hovered":
+          return outlined.hovered;
+
+        case "pressed":
+          return outlined.pressed;
+
+        case "focused":
+          return outlined.focused;
+
+        case "dragged":
+          return outlined.disabled;
+
+        case "disabled":
+          return outlined.disabled;
+      }
+
+      break;
+    }
+  }
+
+  throw new Error("unknown component or options");
+}
+
+const BASE_URL =
+  "https://www.figma.com/design/Ds7hq56K8TtD6dFDe7ZK3y/Material-3-Design-Kit--Community-";
+
+export const md = new MaterialDesignBuilder(
+  BASE_URL,
+);
