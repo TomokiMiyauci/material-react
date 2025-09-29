d422270f8c0f7765d188a821e3e44c6be70efe62
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-09-29 22:26:09 +0900

    feat: export switch component

--- src/mod.ts
+++ src/mod.ts
@@ -2,3 +2,4 @@ export * from "./components/icon_button/mod.ts";
 export * from "./components/navigation_bars/navigation_bar/mod.ts";
 export * from "./components/navigation_bars/navigation_bar_item/mod.ts";
 export * from "./components/badge/mod.ts";
+export * from "./components/switch/mod.ts";
