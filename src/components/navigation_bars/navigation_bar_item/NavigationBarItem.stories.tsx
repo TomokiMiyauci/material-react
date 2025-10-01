107ed4e5578b04efd86b11ba4d515ca5a1de59c6
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-01 20:51:13 +0900

    chore(navigation-bar): add gallary for navigation bar item

--- src/components/navigation_bars/navigation_bar_item/NavigationBarItem.stories.tsx
+++ src/components/navigation_bars/navigation_bar_item/NavigationBarItem.stories.tsx
@@ -1,6 +1,9 @@
-import NavigationBarItem from "./NavigationBarItem.tsx";
+import NavigationBarItem, {
+  type NavigationBarItemProps,
+} from "./NavigationBarItem.tsx";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Badge } from "@miyauci/material-react";
+import { type AxisNode, renderGallary } from "~/.storybook/gallary/mod.ts";
 
 const meta = {
   title: "Example/NavigationBars/NavigationBarItem",
@@ -24,6 +27,143 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
+
+const xAxis = [
+  {
+    label: "Vertical",
+    children: [
+      { label: "Default" },
+      { label: "Active" },
+      {
+        label: "Badge",
+        children: [
+          {
+            label: "Small",
+            children: [
+              { label: "Inactive" },
+              { label: "Active" },
+            ],
+          },
+          {
+            label: "Large",
+            children: [
+              { label: "Inactive" },
+              { label: "Active" },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+] satisfies AxisNode[];
+
+const yAxis = [
+  { label: "Enabled" },
+  { label: "Hovered" },
+  { label: "Focused" },
+  { label: "Pressed" },
+] satisfies AxisNode[];
+
+const matrix: Item[][] = [
+  [
+    { state: "enabled" },
+    { state: "enabled", active: true },
+    { state: "enabled", badge: { size: "small" } },
+    { state: "enabled", active: true, badge: { size: "small" } },
+    { state: "enabled", badge: { size: "large" } },
+    { state: "enabled", active: true, badge: { size: "large" } },
+  ],
+  [
+    { state: "hovered" },
+    { state: "hovered", active: true },
+    { state: "hovered", badge: { size: "small" } },
+    { state: "hovered", active: true, badge: { size: "small" } },
+    { state: "hovered", badge: { size: "large" } },
+    { state: "hovered", active: true, badge: { size: "large" } },
+  ],
+  [
+    { state: "focused" },
+    { state: "focused", active: true },
+    { state: "focused", badge: { size: "small" } },
+    { state: "focused", active: true, badge: { size: "small" } },
+    { state: "focused", badge: { size: "large" } },
+    { state: "focused", active: true, badge: { size: "large" } },
+  ],
+  [
+    { state: "pressed" },
+    { state: "pressed", active: true },
+    { state: "pressed", badge: { size: "small" } },
+    { state: "pressed", active: true, badge: { size: "small" } },
+    { state: "pressed", badge: { size: "large" } },
+    { state: "pressed", active: true, badge: { size: "large" } },
+  ],
+];
+
+type State = "enabled" | "hovered" | "focused" | "pressed";
+
+interface Item {
+  state: State;
+  active?: boolean;
+  badge?: {
+    size?: "large" | "small";
+  };
+}
+
+interface StateProps {
+  "data-hover"?: "";
+  "data-focus"?: "";
+  "data-press"?: "";
+}
+
+function getProps(item: Item): NavigationBarItemProps & StateProps {
+  const base = {
+    icon: (
+      <span className="material-symbols-outlined">
+        home
+      </span>
+    ),
+    label: "Label",
+    active: item.active,
+    badge: item.badge
+      ? item.badge.size === "small" ? <Badge /> : <Badge size="large">3</Badge>
+      : undefined,
+  } satisfies NavigationBarItemProps;
+
+  switch (item.state) {
+    case "enabled":
+      return base;
+    case "hovered":
+      return { ...base, "data-hover": "" };
+    case "focused":
+      return { ...base, "data-focus": "" };
+    case "pressed":
+      return { ...base, "data-press": "" };
+  }
+}
+
+export const Gallary = {
+  render: () =>
+    renderGallary({
+      xAxis,
+      yAxis,
+      matrix,
+    }, {
+      renderData: (item) => {
+        if (item) {
+          const props = getProps(item);
+          return <NavigationBarItem {...props} />;
+        }
+        return;
+      },
+    }),
+  parameters: {
+    pseudo: {
+      hover: ["[data-hover]"],
+      active: ["[data-press]"],
+      focusVisible: ["data-focus"],
+    },
+  },
+} satisfies Story;
 
 export const Enabled = {} satisfies Story;
 export const Hoverd = {
