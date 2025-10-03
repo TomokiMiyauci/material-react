3d27d1597747b392e3e0d7f20091a363404da3f7
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-03 13:21:11 +0900

    feat(button): export button component

--- src/mod.ts
+++ src/mod.ts
@@ -4,3 +4,4 @@ export * from "./components/navigation_bars/navigation_bar_item/mod.ts";
 export * from "./components/badge/mod.ts";
 export * from "./components/switch/mod.ts";
 export * from "./components/tooltips/plain_tooltip/mod.ts";
+export * from "./components/buttons/button/mod.ts";
