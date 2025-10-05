2cd97e19118a3bbe66a8ff7004a63e3caf4d4eb8
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-05 16:41:20 +0900

    refactor(radio): use generated tokens

--- src/components/radio/Radio.stories.tsx
+++ src/components/radio/Radio.stories.tsx
@@ -6,6 +6,7 @@ const meta = {
   component: Radio,
   parameters: {
     layout: "centered",
+    chromatic: { disableSnapshot: false },
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof Radio>;
@@ -26,3 +27,49 @@ export const Hovered = {
     pseudo: { hover: true },
   },
 } satisfies Story;
+
+export const Focused = {
+  parameters: {
+    pseudo: { focusVisible: true },
+  },
+} satisfies Story;
+
+export const Pressed = {
+  parameters: {
+    pseudo: { active: true },
+  },
+} satisfies Story;
+
+export const CheckedEnabled: Story = {
+  args: { checked: true },
+  name: "Checked/Enabled",
+};
+
+export const CheckedDisabled: Story = {
+  args: { checked: true, disabled: true },
+  name: "Checked/Disabled",
+};
+
+export const CheckedHovered: Story = {
+  args: { checked: true },
+  name: "Checked/Hovered",
+  parameters: {
+    pseudo: { hover: true },
+  },
+};
+
+export const CheckedFoucsed: Story = {
+  args: { checked: true },
+  name: "Checked/Focused",
+  parameters: {
+    pseudo: { focusVisible: true },
+  },
+};
+
+export const CheckedPressed: Story = {
+  args: { checked: true },
+  name: "Checked/Pressed",
+  parameters: {
+    pseudo: { active: true },
+  },
+};
