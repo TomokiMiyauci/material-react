f28cf4239a74a03cc0bdc99b36e34e9038a1aae5
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-13 15:05:15 +0900

    feat(badge): change badge component to DSD style

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,9 +1,15 @@
-import type { JSX } from "react";
+import type { JSX, ReactNode } from "react";
 import style from "./_generated/style.ts";
 import Style from "@internal/Style.tsx";
+import Template from "@internal/dsd/Template.tsx";
 
 export interface BadgeProps {
   size?: BadgeSize;
+
+  /**
+   * The text to be displayed.
+   */
+  children?: ReactNode;
 }
 
 type BadgeSize = "small" | "large";
@@ -15,8 +21,14 @@ export default function Badge(
 
   return (
     <>
-      <span data-md="badge" data-size={size} {...rest}>
-        {size === "large" && children}
+      <span data-md="badge" data-size={size}>
+        <Template shadowrootmode="open">
+          <span part="container" {...rest}>
+            <slot part="text"></slot>
+          </span>
+        </Template>
+
+        {children}
       </span>
 
       <Style href="badge">{style}</Style>
