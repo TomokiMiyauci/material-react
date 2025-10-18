a45efa4d988bd8580b2e61c9468d28e046e811a9
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2025-10-18 22:28:38 +0900

    feat: add css as JSX with style tag

--- src/components/styles/color/Color.tsx
+++ src/components/styles/color/Color.tsx
@@ -2,8 +2,8 @@ import type { JSX } from "react";
 import Style from "@internal/Style.tsx";
 import style from "./_generated/style.ts";
 
-export default function Shape(
+export default function Color(
   props: JSX.IntrinsicElements["style"],
 ): JSX.Element {
-  return <Style href="shape" {...props}>{style}</Style>;
+  return <Style href="color" {...props}>{style}</Style>;
 }
--- src/components/styles/typeface/Typeface.tsx
+++ src/components/styles/typeface/Typeface.tsx
@@ -2,8 +2,8 @@ import type { JSX } from "react";
 import Style from "@internal/Style.tsx";
 import style from "./_generated/style.ts";
 
-export default function Shape(
+export default function Typeface(
   props: JSX.IntrinsicElements["style"],
 ): JSX.Element {
-  return <Style href="shape" {...props}>{style}</Style>;
+  return <Style href="typeface" {...props}>{style}</Style>;
 }
